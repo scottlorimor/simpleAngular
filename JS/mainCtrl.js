55681f07c3265c88ce5b8f6ba7e1bc392cc95b7a
scott lorimor <scott.lorimor@cambeo.io>
2017-09-25 20:15:58 -0600

    add ability to add friend with button click

--- JS/mainCtrl.js
+++ JS/mainCtrl.js
@@ -1,4 +1,11 @@
 angular.module('friendsList').controller('mainCtrl', function($scope) {
 	$scope.name = "This is a test"
 	$scope.friends = ["ben", "tom", "kevin", "bob"]
+	$scope.newFriend = ''
+	
+	$scope.addFriend = function(friend) {
+		$scope.friends.push($scope.newFriend);
+		console.log(friend+" was added to array.")
+		$scope.newFriend = ''
+	}
 })
